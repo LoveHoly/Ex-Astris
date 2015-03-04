d8938773669eec1fe99f7dde428bb2ddb2c55ffd
Michael Lydeamore <michael.lydeamore@adelaide.edu.au>
2015-03-04 10:38:14 +1030

    Moved ThermalExpansion methods to CoFHLib stuff, which is much safer.

--- src/main/java/ExAstris/Bridge/ThermalExpansion.java
+++ src/main/java/ExAstris/Bridge/ThermalExpansion.java
@@ -1,8 +1,7 @@
 package ExAstris.Bridge;
 
-import java.lang.reflect.Method;
-
 import ExAstris.Data.ModData;
+import cofh.api.modhelpers.ThermalExpansionHelper;
 import cofh.thermalfoundation.fluid.TFFluids;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -29,23 +28,23 @@ public class ThermalExpansion {
 	{
 		if (Loader.isModLoaded("Forestry"))
 		{
-			addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Blocks.leaves,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,1),null,0,false);
-			addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Blocks.grass,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,2),null,0,false);
-			addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Blocks.sand,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,3),null,0,false);
-			addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Blocks.vine,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,4),null,0,false);
-			addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Items.ender_pearl,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,5),null,0,false);
-			addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Blocks.ice,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,6),null,0,false);
-			addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Blocks.mycelium,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,7),null,0,false);
+			ThermalExpansionHelper.addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Blocks.leaves,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,1));
+			ThermalExpansionHelper.addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Blocks.grass,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,2));
+			ThermalExpansionHelper.addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Blocks.sand,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,3));
+			ThermalExpansionHelper.addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Blocks.vine,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,4));
+			ThermalExpansionHelper.addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Items.ender_pearl,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,5));
+			ThermalExpansionHelper.addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Blocks.ice,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,6));
+			ThermalExpansionHelper.addSmelterRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(Blocks.mycelium,8),new ItemStack(GameRegistry.findBlock("Forestry", "beehives"),1,7));
 		}
 		if (Loader.isModLoaded("ExtraBees"))
 		{
-			addTransposerRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(GameRegistry.findBlock("ExtraBees", "hive"),1,0),new FluidStack(FluidRegistry.WATER, 8000),false);
+			ThermalExpansionHelper.addTransposerFill(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(GameRegistry.findBlock("ExtraBees", "hive"),1,0),new FluidStack(FluidRegistry.WATER, 8000),false);
 			if(Loader.isModLoaded("TConstruct")){
 				//addTransposerRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(GameRegistry.findBlock("ExtraBees", "hive"),1,1),new FluidStack(TinkerSmeltery.moltenStoneFluid, 8000),false);
 			}
-			addTransposerRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(GameRegistry.findBlock("ExtraBees", "hive"),1,2),new FluidStack(TFFluids.fluidRedstone, 8000),false);
+			ThermalExpansionHelper.addTransposerFill(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(GameRegistry.findBlock("ExtraBees", "hive"),1,2),new FluidStack(TFFluids.fluidRedstone, 8000),false);
 			if(Loader.isModLoaded("MineFactoryReloaded")){
-				addTransposerRecipe(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(GameRegistry.findBlock("ExtraBees", "hive"),1,3),new FluidStack(FluidRegistry.getFluid("milk"), 8000),false);
+				ThermalExpansionHelper.addTransposerFill(10000, new ItemStack(GameRegistry.findBlock("exnihilo", "bee_trap_treated")),new ItemStack(GameRegistry.findBlock("ExtraBees", "hive"),1,3),new FluidStack(FluidRegistry.getFluid("milk"), 8000),false);
 			}
 
 		}
@@ -62,53 +61,8 @@ public class ThermalExpansion {
 		}
 
 	}
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static void addSmelterRecipe(int Energy, ItemStack PrimaryInput, ItemStack SecondaryInput, ItemStack PrimaryOutput,ItemStack SecondaryOutput, int SecondaryChance,boolean Overwrite)
-	{
-		Class smelter = null;
-		Method addRecipe = null;
-		
-		Object[] parameters ={(Object)Energy, (Object)PrimaryInput, (Object)SecondaryInput, (Object)PrimaryOutput,(Object)SecondaryOutput,(Object)SecondaryChance, (Object)Overwrite};
-		try {
-			smelter = Class.forName("cofh.thermalexpansion.util.crafting.SmelterManager");
-
-			if (smelter != null)
-			{	
-				addRecipe = smelter.getDeclaredMethod("addRecipe", int.class, ItemStack.class, ItemStack.class, ItemStack.class,ItemStack.class,int.class, boolean.class);
-				addRecipe.setAccessible(true);
-			}
-
-			if (addRecipe != null)
-			{
-				addRecipe.invoke(null, parameters);
-			}
-		}
-		catch (Exception ex){System.out.println("Ex Astris: Failed to add Smelter recipes, " + ex.getMessage());}
-	}
 	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static void addTransposerRecipe(int Energy, ItemStack InputItem, ItemStack Output, FluidStack InputFluid, boolean Overwrite)
-	{
-		Class transposer = null;
-		Method addRecipe = null;
-		
-		Object[] parameters ={(Object)Energy, (Object)InputItem, (Object)Output, (Object)InputFluid, (Object)Overwrite};
-		try {
-			transposer = Class.forName("cofh.thermalexpansion.util.crafting.TransposerManager");
-
-			if (transposer != null)
-			{	
-				addRecipe = transposer.getDeclaredMethod("addTEFillRecipe", int.class, ItemStack.class, ItemStack.class, FluidStack.class, boolean.class);
-				addRecipe.setAccessible(true);
-			}
-
-			if (addRecipe != null)
-			{
-				addRecipe.invoke(null, parameters);
-			}
-		}
-		catch (Exception ex){System.out.println("Ex Astris: Failed to add Transposer recipes, " + ex.getMessage());}
-	}
+	
 	public static void summonBlizz(World worldObj,double x,double y,double z)
 	{
 		EntityBlizz peck = new EntityBlizz(worldObj);
